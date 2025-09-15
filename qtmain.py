5d41f7340e0a54460b5a4205e8112ca87c4110e7
213223094聂云腾 <213223094@seu.edu.cn>
2025-09-15 16:38:35 +0800

    尚未正式启动开发，仅为测试相关文件

---
+++ qtmain.py
@@ -0,0 +1,20 @@
+
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from interface import Ui_Form
+
+class MyMainForm(QMainWindow, Ui_Form):
+    def __init__(self, parent=None):
+        super(MyMainForm, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    #固定的，PyQt程序都需要QApplication对象。sys.argv是命令行参数列表，确保程序可以双击运行
+    app = QApplication(sys.argv)
+    #初始化
+    myWin = MyMainForm()
+    #将窗口控件显示在屏幕上
+    myWin.show()
+    #程序运行，sys.exit方法确保程序完整退出。
+    sys.exit(app.exec())
